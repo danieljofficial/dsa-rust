28ea334d0c97afa04e338edddd9b4effdca8fa05
danieljofficial <joenah004@gmail.com>
2026-03-06 13:35:29 +0100

    note updates

--- problems/longest-palindromic-substring/src/main.rs
+++ problems/longest-palindromic-substring/src/main.rs
@@ -1,22 +1,11 @@
-// use std::{io::Error, num};
-
-// fn main() {
-//     let io_err: Error = Error::last_os_error();
-//     let parse_err: num::ParseIntError = "not a number".parse::<i32>().unwrap_err();
-
-//     let err1: Box<Error> = From::from(io_err);
-//     let err2: Box<Error> = From::from(parse_err);
-// }
-
-use std::error::Error;
-use std::io;
-use std::num;
+fn longest_palindromic_string(s: String) -> String {
+    s.chars().rev().collect()
+}
 
 fn main() {
-    let io_err: io::Error = io::Error::last_os_error();
-    let parse_err: num::ParseIntError = "not a number".parse::<i32>().unwrap_err();
+    let st = String::from("cbbd");
+    let result = longest_palindromic_string(st);
 
-    // OK, here are the conversions.
-    let err1: Box<dyn Error> = From::from(io_err);
-    let err2: Box<dyn Error> = From::from(parse_err);
+    println!("{result}");
+    // assert_eq!(result, String::from("bb"));
 }
